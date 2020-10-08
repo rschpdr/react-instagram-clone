4348de9ef4bdd5b929a902318a4b1dcf52f255f0
Filipe Deboni <deboni.filipe@gmail.com>
2020-10-08 15:03:07 -0300

    footer started

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Footer from "./Footer";
 import Header from "./Header";
 import Timeline from "./Timeline";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
